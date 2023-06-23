1a6fdd0f94d4f4945a022774b7df246a62dbb947
lvillamizarmurillo <lvillamizarmurillo@gmail.com>
2023-06-23 16:02:41 -0500

    feat: :tada: Conexion PDO
    se conecto mediante pdo.

---
+++ scripts/db/credential.php
@@ -0,0 +1,10 @@
+<?php
+    namespace App;
+    abstract class credentials{
+        use system;
+        private $host = "172.16.49.20";
+        private $dbname = "db_M3_prueba_miguel";
+        protected $username = "sputnik";
+        private $password = "Sp3tn1kC@";
+    }
+?>
